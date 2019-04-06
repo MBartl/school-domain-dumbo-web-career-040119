36daaa9cfa0b019b97e05dd61686c2c2724e4fb5
Matthew Bartl <mattbartl@gmail.com>
2019-04-06 00:04:53 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+require 'pry'
+
+class School
+  attr_accessor :school, :roster, :grade
+  
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    if !roster.include?(grade) 
+      roster[grade] = []
+    end
+    roster[grade] << student
+  end
+  
+  def grade(input)
+    @roster[input]
+  end
+
+  def sort
+    
+  end
+end
